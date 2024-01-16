f2da9a9c7d584c3491d8b1892d72df38d96f0e6e
MariuszKuich <mariuszkuich@outlook.com>
2024-01-16 21:35:34 +0100

    initial commit

---
+++ src/test/java/pl/mariuszk/memorizer/MemorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mariuszk.memorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
