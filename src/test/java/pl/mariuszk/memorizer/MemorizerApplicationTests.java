87ddac2ba34e10b293803729c992b2b796b0db40
MariuszKuich <mariuszkuich@outlook.com>
2024-01-16 21:49:04 +0100

    configure H2 in-memory db for tests

--- src/test/java/pl/mariuszk/memorizer/MemorizerApplicationTests.java
+++ src/test/java/pl/mariuszk/memorizer/MemorizerApplicationTests.java
@@ -3,11 +3,13 @@ package pl.mariuszk.memorizer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// Steps for configuring H2 in-memory database for tests:
+// 1. Add com.h2database.h2 dependency with test scope in pom.xml.
+// 2. Configure H2 database in src/test/resources/application.yml properties file.
 @SpringBootTest
 class MemorizerApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
